accd154d7dfb827f9dc61813f4872ef85c191e06
Student <cwalker1332@gmail.com>
2020-10-26 17:49:58 -0500

    added class for each level

---
+++ src/LevelOne.java
@@ -0,0 +1,18 @@
+public class LevelOne {
+
+    // enter questions into appropriate array's squiggly braces
+    // decide whether we want to do a separate array for multiple choice or just include it with question
+    static String[] typesQuestions = {};
+    static String[] typesAnswers = {};
+    static String[] controlFlowQuestions = {};
+    static String[] controlFlowAnswers = {};
+    static String[] loopsQuestions = {};
+    static String[] loopsAnswers = {};
+    static String[] methodCompQuestions = {};
+    static String[] methodCompAnswers = {};
+
+    // methods to retrieve question
+    public static getLevelOneQuestion(){
+
+    }
+}
---
+++ src/LevelTwo.java
@@ -0,0 +1,16 @@
+public class LevelTwo {
+
+    // enter questions into appropriate array's squiggly braces
+    // decide whether we want to do a separate array for multiple choice or just include it with question
+    static String[] gitQuestions = {};
+    static String[] gitAnswers = {};
+    static String[] commandLineQuestions = {};
+    static String[] commandLineAnswers = {};
+    static String[] dataStructureQuestions = {};
+    static String[] dataStructureAnswers = {};
+
+    // methods to retrieve question
+    public static getLevelTwoQuestion(){
+
+    }
+}
