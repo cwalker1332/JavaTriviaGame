cdf378b14cd9828b9700b31d7a8242752ef55083
Student <cwalker1332@gmail.com>
2020-10-26 15:28:53 -0500

    starting scanner

--- src/JavaTrivia.java
+++ src/JavaTrivia.java
@@ -4,21 +4,16 @@ public class JavaTrivia {
 
     public static void main(String[] args) {
         //declare variables to be used
-        // int num1, num2, calc;
 
         // create new Scanner named input
         Scanner input = new Scanner(System.in);
 
-        // instruct user to pick an operand then set operator equal to the operand/char they input
-        System.out.println("Please pick an operand.");
-        int operator = input.nextInt();
+        // instruct user to enter their name
+        System.out.println("Hi! Welcome to the Dream Corps Midterm Trivia Game!" +
+                " Enter your name so we know what to call you.");
+        String name = input.nextLine();
 
-        // instruct user to pick a number, or numbers, to be tested
-        // num1 is equal to the first integer they input
-        // num2 is equal to the second integer they put
-        System.out.println("Please type two numbers.");
-        num1 = input.nextInt();
-        num2 = input.nextInt();
+        System.out.println("Hi " + name + ". Let's get started!");
 
 
     }
