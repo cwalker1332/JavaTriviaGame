6fb9aaa8c2601ea1eec1bf9c4c678cd162218b13
Student <cwalker1332@gmail.com>
2020-10-26 14:35:36 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //declare variables to be used
+        // int num1, num2, calc;
+
+        // create new Scanner named input
+        Scanner input = new Scanner(System.in);
+
+        // instruct user to pick an operand then set operator equal to the operand/char they input
+        System.out.println("Please pick an operand.");
+        int operator = input.nextInt();
+
+        // instruct user to pick a number, or numbers, to be tested
+        // num1 is equal to the first integer they input
+        // num2 is equal to the second integer they put
+        System.out.println("Please type two numbers.");
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+
+
+    }
+}
