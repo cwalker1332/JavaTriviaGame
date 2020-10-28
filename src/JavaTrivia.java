7198e77e8a791ae30a8095e82318b7b9351607da
Student <cwalker1332@gmail.com>
2020-10-28 17:31:52 -0500

    getting structure down

--- src/JavaTrivia.java
+++ src/JavaTrivia.java
@@ -3,11 +3,9 @@ import java.util.Scanner;
 public class JavaTrivia {
 
     public static void main(String[] args) {
-        //declare variables to be used
         //TODO: can potentially do a high score feature
-        int score = 0;
 
-        // create new Scanner named input
+        // create new Scanner object
         Scanner input = new Scanner(System.in);
 
         // instruct user to enter their name
@@ -17,13 +15,10 @@ public class JavaTrivia {
 
         System.out.println("Hi " + name + ". Let's get started!");
 
-        // print out random question from level one
-        System.out.println(LevelOne.getLevelOneQuestion());
+        // start level one
+        LevelOne.startLevelOne();
 
-        //TODO: for loop to run through questions and increase score
-        //TODO: if-else statement within for loop to give option to move on to next level when sufficient points (8) are reached or start over for more questions
-
-        // System.out.println("Good work! Would you like to review some topics or exit session?");
+        System.out.println("Good work! Would you like to review some topics or exit session?");
         // TODO: write if else statement, if they choose 1. Review, then give list of topics to go back over or 2. exit and sout "Good work today! See you next time!"
     }
 }
