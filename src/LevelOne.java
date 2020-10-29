4774acc5eb20451c9f3906d8f63cd5a863fa7837
Student <student@DC-TECH-09.local>
2020-10-29 08:15:17 -0500

    changes

--- src/LevelOne.java
+++ src/LevelOne.java
@@ -6,26 +6,24 @@ public class LevelOne {
     // decide whether we want to do a separate array for multiple choice or just include it with question
     static String[][] typesQna = new String[][]{
             {"What is the proper way to declare a variable?", "A", "A. VariableType variableName;", "B. VariableName;", "C. VariableName variableType;", "D. VariableType;"},
-            {"", "C", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 3?", "A", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 4?", "D", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"}};
+            {"The following code will result in: int num = 6.7;", "D", "A runtime error", "B. Num being 6", "C. Num being 7", "D. A compilation error"},
+            {"Choose the appropriate data type for: 8,675,309", "A", "A. int", "B. char", "C. double", "D. Short"},
+            {"What is the difference between a float and a double?", "A", "A. A float is a real number with single-precision 32-bit, and a double is a real number with double-precision 64-bit.", "B. A double is a real number with single-precision 32-bit, and a float is a real number with float-precision 64-bit."}};
+
     static String[][] controlFlowQna = new String[][]{
-            {"What is the proper way to declare a variable?", "A", "A. VariableType variableName;", "B. VariableName;", "C. VariableName variableType;", "D. VariableType;"},
-            {"", "C", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 3?", "A", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 4?", "D", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"}};
+            {"What type of control flow is defined here? This statement is equivalent to an if-else statement, with one key difference. Of all the blocks of code defined in an if-else statement, only one will be executed. In this statement, multiple blocks of code can be executed.", "B", "A. Ternary Operator Statement;", "B. Switch Statement;"},
+            {"What is the difference between a continue and break statement?", "A", "A. A break statement terminates a loop and a continue statement terminates the current loop iteration", "B. Only a continue statement may have a label", "C. Both can be used with a switch statement", "D. Only a break statement may have a label"}};
 
     static String[][] loopsQna = new String[][]{
-            {"What is the proper way to declare a variable?", "A", "A. VariableType variableName;", "B. VariableName;", "C. VariableName variableType;", "D. VariableType;"},
-            {"", "C", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 3?", "A", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
-            {"Question 4?", "D", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"}};;
+            {"What is the difference between do-while and while statements?", "B", "A. Code within while blocks are always executed at least once before condition is evaluated", "B. Code within do-while blocks are always executed at least once before condition is evaluated"},
+            {"What are the three important components that help determine how many times a for-loop will loop?", "A", "A. Initialization, Termination/Condition, Increment/Decrement ", "B. Initialization, Void, Termination/Condition"}};;
 
     static String[][] methodCompQna = new String[][]{
             {"What is the proper way to declare a variable?", "A", "A. VariableType variableName;", "B. VariableName;", "C. VariableName variableType;", "D. VariableType;"},
             {"", "C", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
             {"Question 3?", "A", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"},
             {"Question 4?", "D", "A. Option 1", "B. Option 2", "C. Option 3", "D. Option 4"}};;
+        //TODO finish methodCompQna
 
     static void demi(){
 
--- src/LevelTwo.java
+++ src/LevelTwo.java
@@ -2,14 +2,20 @@ import java.util.Random;
 
 public class LevelTwo {
 
-    // enter questions into appropriate array's squiggly braces
-    // decide whether we want to do a separate array for multiple choice or just include it with question
-    static String[] gitQna = {};
+        // enter questions into appropriate array's squiggly braces
+        // decide whether we want to do a separate array for multiple choice or just include it with question
+        static String[][] gitQna = new String[][]{
+                {"How do you check the state of your local git repository since your last commit?", "B", "A. git check", "B. git status", "C. git commit", "D. git checkout"},
+                {"What is the command syntax to change to a different branch using Git?", "A", "A. git checkout 'branch name'", "B. git status 'branch name'"}};
 
-    static String[] commandLineQna = {};
+        static String[][] basicCommandLineQna = new String[][]{
+            {"What is a directory?", "A", "A. An organizational file system structure that contains files and/or other directories", "B. An alphabetical list", "C. A book"},
+            {"What is the command line for the calendar in the terminal?", "B", "cd", "B. cal"}};
 
-    static String[] dataStructureQna = {};
+        static String[][] dataStructuresAndAlgorithmsQna = new String[][]{
+            {"What is pseudocode?", "A", "A. Informal descriptions of algorithms", "B. Strict syntax of code"},
 
+            //TODO finish dataStructuresAndAlgorithmsQna
 
     // methods to retrieve question
     public static String[] getLevelTwoQuestion(){
